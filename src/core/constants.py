b663e5677ef43fcfa017f46c5a13d2a998597be3
jakthom <aerialfly@gmail.com>
2024-06-25 09:53:19 -0400

    Stubs connection stuff

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,9 +2,17 @@ import os
 from pathlib import Path
 
 
+# Configuration
 USR_HOME_DIR = os.path.expanduser("~")
-SSH_DIR = Path(USR_HOME_DIR, ".ssh")
 SFCLI_DIR = Path(USR_HOME_DIR, ".sfcli")
+
+
+# Key Pair Generation
+SSH_DIR = Path(USR_HOME_DIR, ".ssh")
 SFCLI_CONFIG_FILE_PATH = Path(USR_HOME_DIR, ".sfcli", "config")
 SFCLI_KEYPAIR_PRIV_NAME = "sfcli.pem"
 SFCLI_KEYPAIR_PUB_NAME = "sfcli.pub"
+
+
+# General
+SFCLI = "snowflakecli"
---
+++ src/core/snowflake/connection.py
@@ -0,0 +1,70 @@
+from contextlib import contextmanager
+from typing import Generator, Optional
+from uuid import uuid4
+from dataclasses import dataclass
+
+from snowflake.conector import DictCursor, connect
+from snowflake.connector.connection import SnowflakeConnection
+from snowflake.connector.cursor import SnowflakeCursor
+
+
+from src.core.util.time import iso_now
+from src.core.constants import SFCLI
+
+
+@dataclass
+class ConnectionParams:
+    """ "An object to represent snowflake connection parameters"""
+
+    account: str
+    user: str
+    private_key: str
+    warehouse: str = None
+    role: str = None
+    query_tag: str = None
+
+
+def snowflake_connection(params: ConnectionParams) -> SnowflakeConnection:
+    """Generate a Snowflake Connection"""
+    connection_id = uuid4()
+    connection = connect(
+        account=params.account,
+        user=params.user,
+        private_key=params.private_key,
+        warehouse=params.warehouse,
+        role=params.role,
+        client_telemetry_enabled=False,
+        session_parameters={
+            "connection_id": connection_id,
+            "connection_start": iso_now(),
+        },
+        client_sesion_keep_alive=True,
+    )
+    connection._telemetry_enabled = False  # pylint: disable=W0212
+    return connection
+
+
+def snowflake_cursor(
+    params: ConnectionParams, query_tag: str = SFCLI
+) -> SnowflakeCursor:
+    """Generate a Snowflake Cursor"""
+    connection = connection(params)
+    cursor = connection.cursor(DictCursor)
+    if query_tag:
+        cursor.execute(f"alter session set query_tag = '{query_tag}';")
+    return cursor
+
+
+@contextmanager
+def cursor(params: ConnectionParams) -> Generator[SnowflakeCursor, None, None]:
+    """A helper context manager used to generate a cursor with some niceties"""
+    cursor = snowflake_cursor(params)
+    try:
+        yield cursor
+        cursor.connection.commit()
+    except Exception:
+        cursor.connection.rollback()
+        raise
+    cursor.close()
+    cursor.connection.close()
+    return
